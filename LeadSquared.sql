920b5a0995351e4584825c8f55197fb976958a29
Zeeshan <47472384+Zeeshan131@users.noreply.github.com>
2020-12-11 18:35:35 +0530

    Update LeadSquared.sql

--- LeadSquared.sql
+++ LeadSquared.sql
@@ -4,27 +4,27 @@ USE EmpInfo;
 
 /*Create Table employee*/
 CREATE TABLE employee (
-	EMPLOYEE_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    EMPLOYEE_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     FIRST_NAME VARCHAR(50) NOT NULL,
     LAST_NAME VARCHAR(50) NOT NULL,
     SALARY INT NOT NULL,
-    JOINING_DATE VARCHAR(100) NOT NULL,
+    JOINING_DATE DATETIME NOT NULL,
     MANAGER INT,
-    DEPARTMENT VARCHAR(70)
+    DEPARTMENT VARCHAR(70) NOT NULL
 );
 
 /*Insert data into employee table*/
 INSERT INTO employee 
 (EMPLOYEE_ID, FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, MANAGER, DEPARTMENT)
 VALUES
-(1, 'John', 'Abraham', 1000000, '01-JAN-13 12.00.00 AM', 3, 'Banking'),
-(2, 'Michael', 'Clarke', 800000, '01-JAN-13 12.00.00 AM', 4, 'Insurance'),
-(3, 'Roy', 'Thomas', 700000, '01-FEB-13 12.00.00 AM', null, 'Banking'),
-(4, 'Tom', 'Jose', 600000, '01-FEB-13 12.00.00 AM', null, 'Insurance'),
-(5, 'Jerry', 'Pinto', 650000, '01-FEB-13 12.00.00 AM', 4, 'Insurance'),
-(6, 'Philip', 'Mathew', 750000, '01-JAN-13 12.00.00 AM', 7, 'Services'),
-(7, 'TestName1', '123', 650000, '01-JAN-13 12.00.00 AM', null, 'Services'),
-(8, 'TestName2', 'Lname%', 600000, '01-FEB-13 12.00.00 AM', 4, 'Insurance');
+(1, 'John', 'Abraham', 1000000, '2013-01-01 12:00:00', 3, 'Banking'),
+(2, 'Michael', 'Clarke', 800000, '2013-01-01 12:00:00', 4, 'Insurance'),
+(3, 'Roy', 'Thomas', 700000, '2013-02-01 12:00:00', null, 'Banking'),
+(4, 'Tom', 'Jose', 600000, '2013-02-01 12:00:00', null, 'Insurance'),
+(5, 'Jerry', 'Pinto', 650000, '2013-02-01 12:00:00', 4, 'Insurance'),
+(6, 'Philip', 'Mathew', 750000, '2013-01-01 12:00:00', 7, 'Services'),
+(7, 'TestName1', '123', 650000, '2013-01-01 12:00:00', null, 'Services'),
+(8, 'TestName2', 'Lname%', 600000, '2013-02-01 12:00:00', 4, 'Insurance');
 
 
 
